6b60744a5a4440699be3212e162351b7897688c1
baju0218 <baju0218@naver.com>
2021-01-18 03:34:42 +0900

    MyCalculator 80%

---
+++ MyCalculator/MyCalculator/Calculator.swift
@@ -0,0 +1,136 @@
+//
+//  Calculator.swift
+//  MyCalculator
+//
+//  Created by 백종운 on 2021/01/15.
+//
+
+import Foundation
+
+enum CalculatorError: Error {
+    case invalidValue(String)
+    case invalidOperation(String)
+}
+
+class Calculator {
+    
+    //MARK: Operations
+    private enum Operation {
+        case Random
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case EqualOperation
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        // Random
+        "Rand" : .Random,
+        
+        // Constant
+        "e" : .Constant(M_E),
+        "π" : .Constant(M_PI),
+        
+        // Unary operation
+        "±" : .UnaryOperation{ -$0 },
+        "%" : .UnaryOperation{ $0 / 100 },
+        "1/x" : .UnaryOperation{ if $0 == 0 { return Double.nan } else { return 1 / $0 } },
+        "x!" : .UnaryOperation{ if $0 != floor($0) || $0 < 0 { return Double.nan } else { return Double((1...max(Int64($0), 1)).reduce(1, *)) } },
+        "x²" : .UnaryOperation{ pow($0, 2) },
+        "x³" : .UnaryOperation{ pow($0, 3) },
+        "²√x" : .UnaryOperation{ pow($0, 1 / 2) },
+        "³√x" : .UnaryOperation{ pow($0, 1 / 3) },
+        "eˣ" : .UnaryOperation{ pow(M_E, $0) },
+        "2ˣ" : .UnaryOperation{ pow(2, $0) },
+        "10ˣ" : .UnaryOperation{ pow(10, $0) },
+        "ln" : .UnaryOperation{ if $0 <= 0 { return Double.nan } else { return log($0) } },
+        "log₂" : .UnaryOperation{ if $0 <= 0 { return Double.nan } else { return log2($0) } },
+        "log₁₀" : .UnaryOperation{ if $0 <= 0 { return Double.nan } else { return log10($0) } },
+        "sin" : .UnaryOperation{ sin($0) },
+        "cos" : .UnaryOperation{ cos($0) },
+        "tan" : .UnaryOperation{ tan($0) },
+        "sinh" : .UnaryOperation{ sinh($0) },
+        "cosh" : .UnaryOperation{ cosh($0) },
+        "tanh" : .UnaryOperation{ tanh($0) },
+        "sin⁻¹" : .UnaryOperation{ asin($0) },
+        "cos⁻¹" : .UnaryOperation{ acos($0) },
+        "tan⁻¹" : .UnaryOperation{ atan($0) },
+        "sinh⁻¹" : .UnaryOperation{ asinh($0) },
+        "cosh⁻¹" : .UnaryOperation{ acosh($0) },
+        "tanh⁻¹" : .UnaryOperation{ atanh($0) },
+        
+        // Binary operation
+        "+" : .BinaryOperation{ $0 + $1 },
+        "-" : .BinaryOperation{ $0 - $1 },
+        "×" : .BinaryOperation{ $0 * $1 },
+        "÷" : .BinaryOperation{ $0 / $1 },
+        "xʸ" : .BinaryOperation{ pow($0, $1) },
+        "yˣ" : .BinaryOperation{ pow($1, $0) },
+        "ʸ√x" : .BinaryOperation{ pow($0, 1 / $1) },
+        "log𝗒" : .BinaryOperation{ if $0 <= 0 || $1 <= 0 || $1 == 1 { return Double.nan } else { return log($0) / log($1) } },
+        
+        // Equal operation
+        "=" : .EqualOperation,
+    ]
+    
+    //MARK: Properties
+    private struct BinaryOperationInfo {
+        var firstOperand: Double
+        var binaryFunction: ((Double, Double) -> Double)
+    }
+    
+    private var binaryOperationInfo: BinaryOperationInfo?
+    private var memory = 0.0
+    
+    
+    //MARK: Methods
+    func performClear() {
+        binaryOperationInfo = nil
+    }
+    
+    func performOperation(number: String?, symbol: String?) throws -> Double {
+        var result: Double
+        
+        // Check invalid input
+        guard let value = Double(number ?? "nil") else {
+            throw CalculatorError.invalidValue(number ?? "nil")
+        }
+        guard let operation = operations[symbol ?? "nil"] else {
+            throw CalculatorError.invalidOperation(symbol ?? "nil")
+        }
+        
+        // Perform operation
+        switch operation {
+        case .Random:
+            result = drand48()
+        case .Constant(let k):
+            result = k
+        case .UnaryOperation(let function):
+            result = function(value)
+        case .BinaryOperation(let function):
+            if binaryOperationInfo == nil {
+                result = value
+                binaryOperationInfo = BinaryOperationInfo(firstOperand: value, binaryFunction: function)
+            }
+            else {
+                result = binaryOperationInfo!.binaryFunction(binaryOperationInfo!.firstOperand, value)
+                binaryOperationInfo = BinaryOperationInfo(firstOperand: result, binaryFunction: function)
+            }
+        case .EqualOperation:
+            if binaryOperationInfo == nil {
+                result = value
+            }
+            else {
+                result = binaryOperationInfo!.binaryFunction(binaryOperationInfo!.firstOperand, value)
+                binaryOperationInfo = nil
+            }
+        case .MemoryOperation:
+            
+            
+            
+            break
+        }
+            
+        return result
+    }
+}
--- MyCalculator/MyCalculator/ViewController.swift
+++ MyCalculator/MyCalculator/ViewController.swift
@@ -10,36 +10,120 @@ import UIKit
 class ViewController: UIViewController {
     
     //MARK: Properties
-    private var model = Model()
-    private var displayValue: Double {
-        get {
-            return Double(display.text!)!
-        }
-        set {
-            display.text = String(newValue)
-        }
-    }
+    private var calculator = Calculator()
+    private var isUserTypingNumber = false
 
     //MARK: IBOutlets
+    @IBOutlet weak var body: UIStackView!
+    @IBOutlet weak var radian: UILabel!
     @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var clearButton: UIButton!
     
     //MARK: Initializers
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        body.layer.cornerRadius = 16
+        body.layer.masksToBounds = true
     }
 
     //MARK: IBActions
-    @IBAction private func pressDigit(_ sender: UIButton) {
-        if displayValue == 0 {
-            display.text = sender.currentTitle!
+    @IBAction func pressClearButton() {
+        switch clearButton.currentTitle {
+        case "AC":
+            calculator.performClear()
+            isUserTypingNumber = false
+            display.text = String(0)
+        case "C":
+            isUserTypingNumber = false
+            display.text = String(0)
+            clearButton.setTitle("AC", for: UIControl.State.normal)
+        default:
+            print("Error in pressClearButton : Invalid clear " + (clearButton.currentTitle ?? "nil"))
+        }
+    }
+    
+    @IBAction func pressNumberButton(_ sender: UIButton) {
+        if isUserTypingNumber {
+            switch sender.currentTitle {
+            case ".":
+                if display.text!.contains(Character(".")) {
+                    break
+                }
+                else {
+                    display.text! += sender.currentTitle!
+                }
+            default:
+                if display.text == "0" {
+                    display.text = sender.currentTitle!
+                }
+                else {
+                    display.text! += sender.currentTitle!
+                }
+            }
         }
         else {
-            display.text = display.text! + sender.currentTitle!
+            switch sender.currentTitle {
+            case ".":
+                display.text = "0"
+            default:
+                display.text = ""
+            }
+            
+            isUserTypingNumber = true
+            display.text! += sender.currentTitle!
+            clearButton.setTitle("C", for: UIControl.State.normal)
         }
     }
-    @IBAction func pressOperation(_ sender: UIButton) {
+    
+    @IBAction func pressOperationButton(_ sender: UIButton) {
+        var result: Double
         
+        // Perform operation
+        do {
+            result = try calculator.performOperation(number: display.text, symbol: sender.currentTitle)
+        }
+        catch CalculatorError.invalidValue(let number) {
+            print("Error in pressOperationButton: Invalid value " + number)
+            return
+        }
+        catch CalculatorError.invalidOperation(let symbol) {
+            print("Error in pressOperationButton: Invalid operation " + symbol)
+            return
+        }
+        catch {
+            print("Error in pressOperationButton: ")
+            return
+        }
+        
+        isUserTypingNumber = false
+        if result.isNaN {
+            display.text = "nan"
+        }
+        else if result.isInfinite {
+            display.text = "infinity"
+        }
+        else if result == floor(result) {
+            display.text = String(format: "%.0lf", result)
+        }
+        else {
+            display.text = String(result)
+        }
+        clearButton.setTitle("C", for: UIControl.State.normal)
+    }
+    
+    @IBAction func pressChangeButton(_ sender: UIButton) {
+        switch sender.currentTitle {
+        case "Rad":
+            radian.text = "Rad"
+            sender.setTitle("Deg", for: UIControl.State.normal)
+        case "Deg":
+            radian.text = ""
+            sender.setTitle("Rad", for: UIControl.State.normal)
+        case "2nd":
+            break
+        default:
+            print("Error in pressChangeButton: ")
+        }
     }
 }
-
