0c04f334a55b18ff0b5160253708521c46e9e831
baju0218 <baju0218@naver.com>
2021-02-20 21:00:43 +0900

    add print 1

--- MyLaboratory/MyLaboratory/ViewController.swift
+++ MyLaboratory/MyLaboratory/ViewController.swift
@@ -2,25 +2,20 @@
 //  ViewController.swift
 //  MyLaboratory
 //
-//  Created by 백종운 on 2021/02/07.
+//  Created by 백종운 on 2021/02/20.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var buttons: [UIButton]!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-    
-    @IBAction func touchButton(_ sender: UIButton) {
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0)).then {
-            
-        }
+
+    func git() {
+        print()
     }
-    
 }
+
